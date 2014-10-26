f36d79239f8bd741159065f413cf1116fd7c218b
Lukas1599 <Lukas153lk@gmail.com>
2014-10-25 23:26:22 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -53,7 +53,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Bot",
+        botName: "BrasIleIríssImosBOT",
         language: "portuguese",
         chatLink: "https://rawgit.com/diegodepira/basicBot/master/lang/pt.json",
         maximumAfk: 60,
